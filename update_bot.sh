3e700a11d1dc55a76712b088f4f74cba57705024
TrashRoach <rfsnboris@gmail.com>
2021-11-09 23:46:48 +0400

    Starting screen in detached mode

--- update_bot.sh
+++ update_bot.sh
@@ -7,4 +7,4 @@ venv/bin/pip install -r requirements.txt
 if screen -list | grep -q "music_bot"; then
   screen -S music_bot -X quit
 fi
-screen -S music_bot venv/bin/python3 run.py
+screen -dmS music_bot venv/bin/python3 run.py
