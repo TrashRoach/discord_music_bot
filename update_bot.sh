af5e05e6b4d74cd224cecbed427dcc59067b054b
TrashRoach <rfsnboris@gmail.com>
2022-02-11 23:23:44 +0400

    fix: pip install

--- update_bot.sh
+++ update_bot.sh
@@ -3,4 +3,7 @@ git reset HEAD --hard
 git clean -fd
 git pull
 
+venv/bin/python -m pip install --upgrade pip setuptools wheel
+venv/bin/pip install -r requirements.txt
+
 sudo -S systemctl restart music_bot
