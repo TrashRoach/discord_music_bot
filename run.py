3cd1fdb6ebb45e49b2c77daee1df13a355a5529e
TrashRoach <rfsnboris@gmail.com>
2023-03-21 14:26:46 +0400

    Add intents.message_content to fix cmd_prefix

--- run.py
+++ run.py
@@ -1,15 +1,15 @@
 import os
 
-from dotenv import load_dotenv
-
 import discord
 from discord.ext import commands
+from dotenv import load_dotenv
 
 from config import BOT_CMD_PREFIX
 from general import setup
 
 intents = discord.Intents.default()
 intents.members = True
+intents.message_content = True
 
 bot = commands.Bot(command_prefix=commands.when_mentioned_or(BOT_CMD_PREFIX), intents=intents)
 setup(bot)
